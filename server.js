e410fb5e5ae60873fe091e565ca827a49e2906cd
rahul_tiwari30 <60029698+Rahulrktiwari@users.noreply.github.com>
2024-09-20 14:33:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -113,22 +113,6 @@ app.get('/get-userdata', async (req, res) => {
 });
 
 
-app.post('/oauth/token', async (req, res) => {
-  try {
-    const { data, error } = await supabase
-      .from('whatfix_onboarding_duplicate') // Replace with your table name
-      .select('*');
-
-    if (error) {
-      throw error;
-    }
-
-    res.json(data);
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Failed to retrieve data', details: error.message });
-  }
-});
 
 
 
